776270feedde3dbb5bf70d50ef17ca9f7960a5d4
noorulhoda <noooralhoda2030@gmail.com>
2022-10-12 01:51:35 +0200

    Update render.js

--- server/services/render.js
+++ server/services/render.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
-const dotenv = require('dotenv');
-dotenv.config( { path : 'config.env'} )
+//const dotenv = require('dotenv');
+//dotenv.config( { path : 'config.env'} )
 const PORT = process.env.PORT
 
 exports.homeRoutes = (req, res) => {
