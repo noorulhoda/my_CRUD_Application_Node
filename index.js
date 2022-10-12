498afb86f941129abfc46fb8fc175e5902f8b5e1
noorulhoda <noooralhoda2030@gmail.com>
2022-10-12 02:04:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const connectDB = require('./server/database/connection');
 const app = express();
 
 //dotenv.config( { path : 'config.env'} )
-const PORT = process.env.PORT || 8080
+//const PORT = process.env.PORT || 8080
 
 // log requests
 app.use(morgan('tiny'));
@@ -32,4 +32,4 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 // load routers
 app.use('/', require('./server/routes/router'))
 
-app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+app.listen(process.env.PORT, ()=> { console.log(`Server is running on http://localhost:${process.env.PORT}`)});
